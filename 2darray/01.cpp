580c3171b380e535b776f6bc022dd33dc7b7a4c9
tushar <contact@gmail.com>
2026-07-01 14:47:35 +0530

    maxum

--- 2darray/01.cpp
+++ 2darray/01.cpp
@@ -1,11 +1,22 @@
 #include<iostream>
+#include<algorithm>
+#include<cmath>
+
 int main(){
     int arr[3][3]={1,2,3,4,5,6,7,8,9};
+    int maxsum=0;
+    int idx=0;
     for(int i=0;i<3;i++){
         int sum=0;
         for(int j=0;j<3;j++){
             sum+=arr[i][j];
         }
-        std::cout<<sum<<std::endl;
+        maxsum=sum;
+        if(sum>=maxsum){
+            maxsum=sum;
+            idx=i;
+        }
+        
     }
+    std::cout<<maxsum<<" "<<idx<<std::endl;
 }
